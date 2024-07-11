14fec3906eb88bd4edcc11b5415e3cffd6a71daf
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-07-11 09:59:06 +0200

    more molecules

---
+++ scripts/00_parse_manually_curated_data.py
@@ -0,0 +1,65 @@
+import os
+import pandas as pd
+import rdkit
+from standardiser import standardise
+from rdkit import RDLogger
+
+root = os.path.dirname(os.path.abspath(__file__))
+
+RDLogger.DisableLog('rdApp.*')                                                                                                                                                           
+
+data_dir = os.path.join(root, '..', 'data')
+np_dir = os.path.join(data_dir, 'all', 'NP2')
+sd_dir = os.path.join(data_dir, 'all', 'SD2')
+
+def molecule_loader(subfolder):
+    sdf_paths = []
+    mol_paths = []
+    mol2_paths = []
+    names = []
+    for fn in os.listdir(subfolder):
+        if fn.endswith(".sdf"):
+            sdf_paths.append(os.path.join(subfolder, fn))
+            names += [fn[:-4]]
+        if fn.endswith(".mol"):
+            mol_paths.append(os.path.join(subfolder, fn))
+            names += [fn[:-4]]
+        if fn.endswith(".mol2"):
+            mol2_paths.append(os.path.join(subfolder, fn))
+            names += [fn[:-5]]
+    mols = []
+    for sdf_path in sdf_paths:
+        mols.append(rdkit.Chem.SDMolSupplier(sdf_path))
+    for mol_path in mol_paths:
+        mols.append(rdkit.Chem.MolFromMolFile(mol_path))
+    for mol2_path in mol2_paths:
+        mols.append(rdkit.Chem.MolFromMol2File(mol2_path))
+    mols_ = []
+    c = 0
+    for i, mol in enumerate(mols):
+        try:
+            mol = standardise.run(mol)
+            if mol is not None:
+                mols_ += [(names[i], mol)]
+        except:
+            c += 1
+            continue
+    print("Number of non-standardized molecules (skipped) {0}".format(c))
+    return mols_
+
+np_mols = molecule_loader(sd_dir)
+sd_mols = molecule_loader(np_dir)
+
+def mols_to_table(mols, category):
+    mols_ = []
+    for name, mol in mols:
+        mols_ += [(name, rdkit.Chem.MolToInchiKey(mol), rdkit.Chem.MolToSmiles(mol), category)]
+    df = pd.DataFrame(mols_, columns=['file_name', "inchikey", 'smiles', "category"])
+    return df
+
+np_df = mols_to_table(np_mols, "natural")
+sd_df = mols_to_table(sd_mols, "synthetic")
+
+df = pd.concat([np_df, sd_df]).drop_duplicates().reset_index(drop=True)
+
+df.to_csv(os.path.join(data_dir, 'all_molecules.csv'), index=False)
---
+++ scripts/01_cheese_similarity_search.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+import time
+from rdkit import Chem
+import requests
+
+root = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv(os.path.join(root, "..", ".env"))
+CHEESE_API_KEY = os.getenv("CHEESE_API_KEY")
+
+
+def _query_molecule(smiles, search_type, search_quality, n_neighbors):
+    inchikey = Chem.MolToInchiKey(Chem.MolFromSmiles(smiles))
+    api_key = CHEESE_API_KEY
+    data = requests.get("https://api.cheese.themama.ai/molsearch",
+                        {"search_input": smiles,
+                        "search_type": search_type,
+                        "n_neighbors": n_neighbors,
+                        "search_quality": search_quality,
+                        "descriptors": False,
+                        "properties": False,
+                        "filter_molecules": True},
+                        headers={'Authorization': f"Bearer {api_key}"},
+                        verify=False).json()
+    R = []
+    for r in data["neighbors"]:
+        identifier = r["zinc_id"]
+        if identifier.startswith("ZINC"):
+            database = "zinc15"
+        elif identifier.startswith("ENAMINE"):
+            database = "enamine-real"
+        else:
+            database = None
+        R += [[smiles, inchikey, r["smiles"], identifier, search_type, r["Morgan Tanimoto"], database]]
+    df = pd.DataFrame(R, columns=["query_smiles", "query_inchikey", "smiles", "identifier", "search_type", "score", "database"]).sort_values("score", ascending=False).reset_index(drop=True)
+    return df
+
+
+def query_molecule(smiles, search_type="consensus", search_quality="very accurate", n_neighbors=100):
+    for _ in range(10):
+        try:
+            df = _query_molecule(smiles, search_type=search_type, search_quality=search_quality, n_neighbors=n_neighbors)
+            if df.shape[0] == n_neighbors:
+                return df
+        except:
+            print("Error, retrying in 5 seconds")
+            time.sleep(5)
+
+
+def query_molecule_all_similarities(smiles, search_quality="very accurate", n_neighbors=100):
+    search_types = ["consensus", "morgan", "espsim_electrostatic", "espsim_shape"]
+    dfs = []
+    for search_type in search_types:
+        print(smiles, search_type)
+        dfs += [query_molecule(smiles, search_type=search_type, search_quality=search_quality, n_neighbors=n_neighbors)]
+    df = pd.concat(dfs).reset_index(drop=True)
+    return df
+
+
+def query_and_write(smiles):
+    inchikey = Chem.MolToInchiKey(Chem.MolFromSmiles(smiles))
+    file_name = os.path.join("..", "results", "cheese", f"{inchikey}.csv")
+    if os.path.exists(file_name):
+        print("Already done for", inchikey)
+        return
+    df = query_molecule_all_similarities(smiles)
+    df.to_csv(file_name, index=False)
+
+smiles_list = pd.read_csv(os.path.join(root, "../data/all_molecules.csv"))["smiles"].tolist()
+
+for smiles in smiles_list:
+    query_and_write(smiles)
